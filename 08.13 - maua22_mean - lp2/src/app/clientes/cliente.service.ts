9beb86db3da1b5aae9392f79c46892529ad44567
IGOR EIKI FERREIRA KUBOTA <19.02466-5@maua.br>
2022-08-13 11:08:25 -0300

    update clientes (errors)

--- 08.13 - maua22_mean - lp2/src/app/clientes/cliente.service.ts
+++ 08.13 - maua22_mean - lp2/src/app/clientes/cliente.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from "@angular/core";
 import { Cliente } from "./cliente.model";
 import { Subject } from "rxjs";
 import { HttpClient } from "@angular/common/http";
+import { map } from 'rxjs/operators';
+
 
 @Injectable({ providedIn: 'root' })
 export class ClienteService {
@@ -11,10 +13,21 @@ export class ClienteService {
   constructor(private httpClient: HttpClient){}
 
   getClientes(): void {
-    this.httpClient.get<{mensagem: string,
-      clientes: Cliente[]}>('http://localhost:3000/api/clientes').subscribe(
-        (dados) => {
-          this.clientes = dados.clientes;
+    this.httpClient.get<{mensagem: string, clientes: any}>('http://localhost:3000/api/clientes')
+      .pipe(map((dados) => {
+        return dados.clientes.map(cliente => {
+          return {
+          id: cliente._id,
+          nome: cliente.nome,
+          fone: cliente.fone,
+          email: cliente.email
+          }
+          })
+         
+      }))
+      .subscribe(
+        (clientes) => {
+          this.clientes = clientes;
           this.listaClientesAtualizada.next([...this.clientes]);
         }
       )
