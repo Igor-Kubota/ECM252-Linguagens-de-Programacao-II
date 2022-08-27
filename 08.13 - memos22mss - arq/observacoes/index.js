2e4c122e6ab56709efe90f2306260e0032cd97ec
Igor Kubota <igor-eiki@hotmail.com>
2022-08-27 09:13:04 -0300

    finalizada aula 08.17 arq

--- 08.13 - memos22mss - arq/observacoes/index.js
+++ 08.13 - memos22mss - arq/observacoes/index.js
@@ -1,25 +1,45 @@
-const axios = require ('axios');
+const axios = require('axios');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
 
 const observacoesPorLembreteId = {}
 
+const funcoes = {
+    ObservacaoClassificada: (observacao) => {
+        const observacoes = observacoesPorLembreteId[observacao.lembreteId];
+        const obsParaAtualizar = observacoes.find(o => o.id === observacao.id)
+        obsParaAtualizar.status = observacao.status;
+        axios.post('http://localhost:10000/eventos', {
+            tipo: "ObservacaoAtualizada",
+            dados: {
+                id: observacao.id,
+                texto: observacao.texto,
+                lembreteId: observacao.lembreteId,
+                status: observacao.status
+            }
+        });
+    }
+}
+
 const app = express();
 app.use(bodyParser.json());
 
 app.post('/lembretes/:id/observacoes', async (req, res) => {
     const idObs = uuidv4();
-    const {texto} = req.body;
+    const { texto } = req.body;
 
-    const observacoesDoLembrete = 
+    const observacoesDoLembrete =
         observacoesPorLembreteId[req.params.id] || [];
-    observacoesDoLembrete.push({id: idObs, texto});
+    observacoesDoLembrete.push({ id: idObs, texto, status: 'aguardando' });
     observacoesPorLembreteId[req.params.id] = observacoesDoLembrete;
     await axios.post('http://localhost:10000/eventos', {
         tipo: "ObservacaoCriada",
         dados: {
-            id: idObs, texto, lembreteId: req.params.id
+            id: idObs,
+            texto,
+            lembreteId: req.params.id,
+            status: 'aguardando'
         }
     })
     res.status(201).send(observacoesDoLembrete);
@@ -30,9 +50,9 @@ app.get('/lembretes/:id/observacoes', (req, res) => {
 });
 
 app.post("/eventos", (req, res) => {
-    console.log(req.body);
-    res.status(200).send({msg: "ok"});
-})
+    funcoes[req.body.tipo](req.body.dados);
+    res.status(200).send({ msg: "ok" });
+});
 
 app.listen(5000, () => {
     console.log('Lembretes. Porta 5000.')
