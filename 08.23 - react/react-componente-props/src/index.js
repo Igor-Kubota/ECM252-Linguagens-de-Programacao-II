f86dce3c1fe6eb77475357a07373ebedefed18a9
Igor Kubota <igor-eiki@hotmail.com>
2022-08-27 11:58:21 -0300

    started apostila react 4

---
+++ 08.23 - react/react-componente-props/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+const App = () => {
+ return <div>Um componente</div>
+}
+ReactDOM.render(
+ <App />,
+ document.querySelector('#root')
+)
