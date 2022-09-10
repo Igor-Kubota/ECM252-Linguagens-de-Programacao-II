7118d61cbe1272c3c08d57727456193a498a2739
Igor Kubota <igor-eiki@hotmail.com>
2022-09-10 08:01:22 -0300

    Added changes from 09.03

--- 08.13 - memos22mss - arq/classificacao/index.js
+++ 08.13 - memos22mss - arq/classificacao/index.js
@@ -1,25 +1,27 @@
-const express = require("express");
 const axios = require("axios");
+const express = require("express");
 const app = express();
-
 app.use(express.json());
 
-const palavraChave = "importante"
+const palavraChave = "importante";
 const funcoes = {
     ObservacaoCriada: (observacao) => {
-        observacao.status =
+        observacao.status = 
             observacao.texto.includes(palavraChave)
-                ? "importante"
-                : "comum";
+            ? "importante"
+            : "comum";
         axios.post("http://localhost:10000/eventos", {
             tipo: "ObservacaoClassificada",
             dados: observacao,
         });
     },
 };
-app.post("/eventos", (req, res) => {
-    funcoes[req.body.tipo](req.body.dados);
-    res.status(200).send({ msg: "ok" });
+
+app.post('/eventos', (req, res) =>{
+    try {
+        funcoes[req.body.tipo](req.body.dados);
+    } catch (err) {}
+    res.status(200).send({msg: 'ok'});
 });
 
 app.listen(7000, () => console.log("Classificação. Porta 7000"));
--- 08.13 - memos22mss - arq/observacoes/index.js
+++ 08.13 - memos22mss - arq/observacoes/index.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+const axios = require ('axios');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
@@ -7,10 +7,12 @@ const observacoesPorLembreteId = {}
 
 const funcoes = {
     ObservacaoClassificada: (observacao) => {
-        const observacoes = observacoesPorLembreteId[observacao.lembreteId];
-        const obsParaAtualizar = observacoes.find(o => o.id === observacao.id)
+        const observacoes = 
+            observacoesPorLembreteId[observacao.lembreteId];
+        const obsParaAtualizar = observacoes.find(o => o.id === 
+            observacao.id);
         obsParaAtualizar.status = observacao.status;
-        axios.post('http://localhost:10000/eventos', {
+        axios.post("http://localhost:10000/eventos", {
             tipo: "ObservacaoAtualizada",
             dados: {
                 id: observacao.id,
@@ -27,17 +29,17 @@ app.use(bodyParser.json());
 
 app.post('/lembretes/:id/observacoes', async (req, res) => {
     const idObs = uuidv4();
-    const { texto } = req.body;
+    const {texto} = req.body;
 
-    const observacoesDoLembrete =
+    const observacoesDoLembrete = 
         observacoesPorLembreteId[req.params.id] || [];
-    observacoesDoLembrete.push({ id: idObs, texto, status: 'aguardando' });
+    observacoesDoLembrete.push({id: idObs, texto, status: 'aguardando'});
     observacoesPorLembreteId[req.params.id] = observacoesDoLembrete;
     await axios.post('http://localhost:10000/eventos', {
         tipo: "ObservacaoCriada",
         dados: {
-            id: idObs,
-            texto,
+            id: idObs, 
+            texto, 
             lembreteId: req.params.id,
             status: 'aguardando'
         }
@@ -50,10 +52,12 @@ app.get('/lembretes/:id/observacoes', (req, res) => {
 });
 
 app.post("/eventos", (req, res) => {
-    funcoes[req.body.tipo](req.body.dados);
-    res.status(200).send({ msg: "ok" });
-});
+    try{
+        funcoes[req.body.tipo](req.body.dados);
+    } catch (err) {}
+    res.status(200).send({msg: "ok"});
+})
 
 app.listen(5000, () => {
-    console.log('Lembretes. Porta 5000.')
+    console.log('Observações. Porta 5000.')
 });
