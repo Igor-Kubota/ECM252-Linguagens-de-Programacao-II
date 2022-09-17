49b6c8974935ac997326eb71bc1f6f6e0628464d
Igor Kubota <igor-eiki@hotmail.com>
2022-09-17 12:23:09 -0300

    ap 7 - 2.12

--- 08.23 - react/listas/src/components/App.js
+++ 08.23 - react/listas/src/components/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Busca from './Busca'
 import env from 'react-dotenv'
+import { createClient } from 'pexels'
+import ListaImagens from './ListaImagens'
+import PexelsLogo from './PexelsLogo'
 
-class App extends Component{
+export default class App extends React.Component {
+    pexelsClient = null
+
+    state = { pics: [] }
+
+    componentDidMount() {
+        this.pexelsClient = createClient(env.PEXELS_KEY)
+    }
+
     onBuscaRealizada = (termo) => {
-        console.log(termo)
-        }
-
-    render(){
+        this.pexelsClient.photos.search({
+            query: termo
+        })
+            .then(pics => this.setState({ pics: pics.photos }))
+    }
+    render() {
         console.log(env.PEXELS_KEY)
         console.log(window.env.PEXELS_KEY)
         return (
             <div className='grid justify-content-center m-auto w-9 border-round border-1 border-400'>
                 <div className='col-12'>
-                    <h1>Exibir uma lista de...</h1>
+                    <PexelsLogo/>
                 </div>
-
+                <div className='col-12'>
+                    <h1 className='text-center'>Exibir uma lista de...</h1>
+                </div>
                 <div className='col-8'>
                     <Busca onBuscaRealizada={this.onBuscaRealizada} />
                 </div>
-
+                <div className="col-8">
+                    <ListaImagens pics = {this.state.pics}/>
+                </div>
             </div>
         )
     }
 }
-
-export default App
