b4a3c8e8082271340e418526f2ceeaa8488b6cd7
Igor Kubota <igor-eiki@hotmail.com>
2022-10-01 12:21:14 -0300

    apostila 08 - 2.15
    Aula não finalizada

---
+++ 08.23 - react/hooks-react/src/components/Busca.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import { InputText } from 'primereact/inputtext'
+//import axios from 'axios'
+
+
+const Busca = () => {
+    const [termoDeBusca, setTermoDeBusca] = useState('')
+        useEffect (() => {
+        console.log ("executando todas as vezes...")
+        })
+        useEffect(() => {
+        console.log("executando somente uma vez")
+        }, [])
+        useEffect(() => {
+        console.log("Executando somente quando o termo é alterado")
+        }, [termoDeBusca])
+    
+
+    return (
+        <div>
+            <span className="p-input-icon-left">
+                <i className="pi pi-search"></i>
+                <InputText  onChange={(e) => setTermoDeBusca(e.target.value)} />
+            </span>
+        </div>
+    )
+}
+export default Busca
